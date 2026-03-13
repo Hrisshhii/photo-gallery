c1b2861d98e0b6167be3c485cdb4e756f0cfd4fb
Hrishi <rathodhrishi12345@gmail.com>
2026-03-13 15:11:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <h1 className="text-5xl text-black font-bold text-center mt-20">
+      Tailwind v4 Works
+    </h1>
+  )
+}
+
+export default App
